7473b1c39357b9733f3bac167d6809bda2209086
tahelka <73593127+tahelka@users.noreply.github.com>
2023-05-28 22:05:17 +0300

    uploading initial project

---
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Delegates
+{
+    class MenuItem
+    {
+        public event Action<MenuItem> ReportSelectedItems;
+        private List<MenuItem> m_SubMenu;
+        public string MenuTitle { get; set; }
+
+        private void notifySelectedItems()
+        {
+            if (ReportSelectedItems != null)
+            {
+                ReportSelectedItems.Invoke(m_ID);
+
+                /// which does this:
+                ///foreach (ReportSickDelegate observerDelegate in m_ReportSickDelegates.GetInvocationList())
+                ///{
+                ///    observerDelegate.Invoke(m_ID);SelectedItems
+                ///}
+            }
+        }
+    }
+}
